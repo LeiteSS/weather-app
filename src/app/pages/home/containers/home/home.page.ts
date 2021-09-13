04b420432c5f80105c326217239139faae87a588
LeiteSS <estoudente.program@gmail.com>
2021-09-13 11:31:05 -0300

    feat: Adicionar e Remover dos Favoritos
    Ao pesquisar um cidade é possivel adiciona-la aos Favoritos
    e no card com a previsão do tempo pesquisar é possivel remove-la.
    Entretanto, na pagina de favitos é possivel a cidade e adicionada e
    um 'X' para remove-la dali.

--- src/app/pages/home/containers/home/home.page.ts
+++ src/app/pages/home/containers/home/home.page.ts
@@ -2,13 +2,14 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
 import { select, Store } from '@ngrx/store';
-import { Observable, Subject } from 'rxjs';
+import { combineLatest, Observable, Subject } from 'rxjs';
+import { takeUntil, map } from 'rxjs/operators';
 
 import { CityWeather } from '../../../../shared/models/weather.model';
 import { Bookmark } from '../../../../shared/models/bookmark.model';
 import * as fromHomeActions from '../../state/home.actions';
 import * as fromHomeSelectors from '../../state/home.selectors';
-import { takeUntil } from 'rxjs/operators';
+import * as fromBookmarksSelectors from '../../../bookmarks/state/bookmarks.selectors';
 
 @Component({
   selector: 'app-home',
@@ -17,9 +18,13 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class HomePage implements OnInit, OnDestroy {
 
+  cityWeather$: Observable<CityWeather>;
   cityWeather: CityWeather;
   loading$: Observable<boolean>;
   error$: Observable<boolean>;
+
+  bookmarksList$: Observable<Bookmark[]>;
+  isCurrentFavorite$: Observable<boolean>;
 
   searchControl: FormControl;
 
@@ -32,21 +37,32 @@ export class HomePage implements OnInit, OnDestroy {
   ngOnInit() {
     this.searchControl = new FormControl('', Validators.required);
 
+    this.cityWeather$ = this.store.pipe(select(fromHomeSelectors.selectCurrentWeather));
 
-    this.store
-        .pipe(
-            select(fromHomeSelectors.selectCurrentWeather),
-            takeUntil(this.componentDestroyed$),
-          )
+    this.cityWeather$
+        .pipe(takeUntil(this.componentDestroyed$))
         .subscribe(value => this.cityWeather = value);
 
     this.loading$ = this.store.pipe(select(fromHomeSelectors.selectCurrentWeatherLoading));
     this.error$ = this.store.pipe(select(fromHomeSelectors.selectCurrentWeatherError));
+
+    this.bookmarksList$ = this.store.pipe(select(fromBookmarksSelectors.selectBookmarksList));
+
+    this.isCurrentFavorite$ = combineLatest([this.cityWeather$, this.bookmarksList$])
+      .pipe(
+        map(([current, bookmarksList]) => {
+          if (!!current) {
+            return bookmarksList.some(bookmark => bookmark.id === current.city.id);
+          }
+          return false;
+        }),
+      );
   }
 
   ngOnDestroy() {
     this.componentDestroyed$.next();
     this.componentDestroyed$.unsubscribe();
+    this.store.dispatch(fromHomeActions.clearHomeState());
   }
 
 
@@ -62,5 +78,7 @@ export class HomePage implements OnInit, OnDestroy {
     bookmark.name = this.cityWeather.city.name;
     bookmark.country = this.cityWeather.city.country;
     bookmark.coord = this.cityWeather.city.coord;
+
+    this.store.dispatch(fromHomeActions.toggleBookmark({ entity: bookmark }));
   }
 }
